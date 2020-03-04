40f9cfe6fce566bf335070e2e867d5da492c83b3
Andres Martin <andres.pena@yello.co>
2020-03-04 11:29:30 -0500

    [add] add creation for new users on reactcomponet CreateUser

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
-import { from } from 'apollo-boost';
 
 const CREATE_USER = gql`
   mutation CreateUser($name: String!, $email: String!) {
@@ -36,13 +35,36 @@ class CreateUser extends Component {
         update={this.props.onCreateUser}
       >
         {createUserMutation => (
-          <div className="lg:fixed bottom-o left-0 w-full bg-white border-t border-gray-300">
+          <div className="lg:fixed bottom-0 left-0 w-full bg-white border-t border-gray-300">
             <form className="lg:px-8 pt-2 pb-2" onSubmit={e => this.onSubmit(e, createUserMutation)}>
-              <div className="lg:flex flex-wrap flex-between items-center justify-center lg:p-0 p-6"></div>
+              <div className="lg:flex flex-wrap flex-between items-center justify-center lg:p-0 p-6">
+                <h4 className="font-bold lg:pr-4">Create new user</h4>
+                <div className="lg:pr-4">
+                  <input
+                    className="border rounded w-full py-2 px-3"
+                    type="text"
+                    value={this.state.name}
+                    placeholder="Name"
+                    onChange={e => this.setState({ name: e.target.value })} />
+                </div>
+                <div className="lg:pr-4">
+                  <input
+                    className="border rounded w-full py-2 px-3"
+                    type="text"
+                    value={this.state.email}
+                    placeholder="Email"
+                    onChange={e => this.setState({ email: e.target.value })} />
+                </div>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded" type="submit">
+                  Create User
+                </button>
+              </div>
             </form>
           </div>
         )}
       </Mutation>
-    )
+    );
   }
 }
+
+export default CreateUser;
